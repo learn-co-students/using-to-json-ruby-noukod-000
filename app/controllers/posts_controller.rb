44950249c68f77dd3938b568cb7c640f206d8ea8
Josue Abraham <josuea@noukod.com>
2019-03-12 15:00:03 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,18 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: %i[show edit update]
 
   def index
     @posts = Post.all
   end
 
   def show
-  end
+    @post = Post.find(params[:id])
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @post.to_json(only: [:title, :description, :id],
+                              include: [author: { only: [:name]}]) }
+    end
+  end 
 
   def new
     @post = Post.new
@@ -18,8 +24,7 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     @post.update(post_params)
@@ -28,10 +33,13 @@ class PostsController < ApplicationController
 
   def post_data
     post = Post.find(params[:id])
-    render json: PostSerializer.serialize(post)
+    render json: post.to_json(only: [:title, :description, :id],
+                              include: [author: {only: [:name]}])
+
   end
 
-private
+  private
+
   # Use callbacks to share common setup or constraints between actions.
   def set_post
     @post = Post.find(params[:id])
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,48 +1,13 @@
 require 'rails_helper'
 
-describe 'navigate' do
+describe 'post_path' do
   before do
-    @post = Post.create(title: "My Post", description: "My post desc")
+    @author = Author.create(name: 'Cormac McCarthy')
+    @post = Post.create(title: 'My Post', description: 'My post desc', author: @author)
   end
 
-  it 'shows the title on the show page in a h1 tag' do
+  it 'responds correctly' do
     visit post_path(@post)
-    expect(page).to have_css("h1", text: "My Post")
-  end
-
-  it 'to post pages' do
-    visit post_path(@post)
-    expect(page.status_code).to eq(200)
-  end
-
-  it 'shows the description on the show page in a p tag' do
-    visit post_path(@post)
-    expect(page).to have_css("p", text: "My post desc")
-  end
-end
-
-describe 'form' do
-  it 'shows a new form that submits content and redirects and prints out params' do
-    visit new_post_path
-
-    fill_in 'title', with: "My post title"
-    fill_in 'description', with: "My post description"
-
-    click_on "Submit Post"
-
-    expect(page).to have_content("My post title")
-  end
-
-  it 'shows a new form that submits content and redirects and prints out params' do
-    @post = Post.create(title: "My Post", description: "My post desc")
-
-    visit edit_post_path(@post)
-
-    fill_in 'post[title]', with: "My edit"
-    fill_in 'post[description]', with: "My post description"
-
-    click_on "Update Post"
-
-    expect(page).to have_content("My edit")
+    expect(page.status_code).to eq(200), 'post_path is not responding correctly'
   end
 end
